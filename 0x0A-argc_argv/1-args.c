927259def4260fded33cad6b6a43635251421f4a
eregbene <benedicteregbene@gmail.com>
2022-09-30 14:25:57 -0700

    argc fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
-#include "main.h"
 #include <stdio.h>
-#define UNUSED(x) (void)(x)
+#include "main.h"
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-  UNUSED(argv);
   printf("%d\n", argc - 1);
   return (0);
 }
